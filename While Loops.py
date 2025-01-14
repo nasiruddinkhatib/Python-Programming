49ed9d49d263439d5e752d18c98c5c5cc04f9a50
Nasiruddin Khatib <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-01-14 16:56:53 +0530

    Add files via upload
    While Loops

---
+++ While Loops.py
@@ -0,0 +1,25 @@
+# 1 print Numbers from 1 to 10 using while loop
+# x =1                 # We Will Start number from 1
+# while x<11:           # x will iterate check the condition 11 <= 10
+#     print(x)
+#     x += 1            # 10=10+1=11 False
+
+# 2 print name 5 times using while loop
+# name = input("Enter Your name : ")
+# x = 0
+# while x<6:     # x<=5= true
+#     print(name)
+#     x += 1         # 0 = 0 + 1 = 1 again  1 = 1+1 2 again go in while loop 4 = 4+1=5 again 5=5+1=6 False
+
+# 3 Print the table using a While Loop
+# number = int(input("Enter the number : "))
+# x = 1
+# while x <=10:
+#     print(f" {number} * {x} = {number * x}  ")
+#     x =  x +1
+
+# By using for loop
+a = 2
+for i in range(1,11):
+    print(f"{a}*{i}={a * i}")
+
