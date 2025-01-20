4b76007018a27cc972a85cb7cafada427f87e315
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-01-20 15:19:55 +0530

    Add files via upload
     Basic Undestanding of Lists Should To know before Advanced concepts

---
+++ Basic_List_concepts.py
@@ -0,0 +1,36 @@
+# What is List in python ?
+# A list in Python is a collection of items that are ordered, changeable (mutable), and allow duplicate values.
+# Lists are one of the most commonly used data types in Python because they are versatile and easy to use.
+#The first item has an index of 0, the second has an index of 1, and so on.
+
+numbers = [10,0,-1,7,10,85]
+#names=["nasir","Python","Java","Django"]
+#mix_list=["school",1,"college",2,"Graduate",8.1,"complete"] # So List can contain multiple data type in it
+#print(len(numbers))  # len is use to calculate tha length of data present in it
+#print(max(numbers))   # Give the largest number present in numbers list
+#print(min(numbers))   # Give tha Smallest number
+#print(numbers.sort()) # It will give You None so don't code in this type
+#numbers.sort()        # Code like this to sort numbers
+#print(numbers)
+#numbers.reverse()      # Code for reverse list you also use -1 either to reverse
+#print(numbers)
+#numbers.append(50)      # this function will add 50 at end in numbers
+#print(numbers)
+# if you want to add numbers at specific index then mention index number
+#numbers.insert(1,30) # Insert functions is used to add numbers at any index append only add data at end
+#print(numbers)
+#print(numbers[0:5])   # Print from index 0 till 5  Output : 10,0,-1,7,10
+#print(numbers[1:5])   # print from index 1 till 5  Output : 0,-1,7,10
+#print(numbers[0:5:2]) # it will jump 2 step Output : 10, -1, 10
+#print(numbers[:])     # it will print all the data of the list
+#print(numbers[-1])    # Print from backward
+#print((numbers[1:4])  # Index will start from 1 to 3
+#if You want to add more number use extend function but it will only add at End side
+# numbers.extend([40,50,90,100])
+# print(numbers)
+# numbers.remove(0)  # It will remove the number
+# print(numbers)
+# numbers.pop()    # Pop Will remove the number of the end
+# print(numbers)
+# numbers.clear()    # clear All The list
+# print(numbers)
