fb5fbe94a62f0b4f2638c2b6c3e3df6834f81d4f
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-01-24 23:15:39 +0530

    Basic_Tuples
    A tuple is a collection type in Python that is ordered and immutable. It is commonly used for data that should not change once assigned.

---
+++ tuples.py
@@ -0,0 +1,34 @@
+# What is tuples ?
+# A tuple is a collection which is ordered and unchangeable.
+# Tuples are written with round brackets().
+# Tuple items are indexed, the first item has index [0], the second item has index [1] etc
+# Tuple items are ordered, unchangeable, and allow duplicate values.
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary,
+# all with different qualities and usage.
+# Realtime Uses of tuple  Will Like Storing geographical co-ordinates
+
+#t1 = ("nasir",1,True,1.1,100,"Boss")   # tuple 1
+#t2 =(2,3,4,50,90,4)                    # tuple 2
+#print(type(t1))       # To check type
+# print(t1[0])         # It will Print index 0  Output: nasir
+# print(t1[1:3])
+#print(t1+t2)
+# print(max(t2))        # It will print Max Number
+# print(min(t2))        # Minimum Number
+
+# *****Tuple  Deletion****
+# t3 = ("nasir",1,2)
+# del t3               # It will Delete tuple Del Keyword is used here simple.
+
+
+
+# **********Tuple Deletion*********
+# t3 = ("nasir", 1, 2)
+# print("Before deletion:", t3)
+#
+# del t3  # Deletes the tuple
+# # Trying to print t3 after deletion will raise an error
+# try:
+#     print(t3)
+# except NameError as e:
+#     print("Error:", e)
