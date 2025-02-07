785ac5c2959c66a33a48b7518c2a7375f2d0a17b
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-02-07 10:47:22 +0530

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,34 +1,34 @@
 # print fibonacci series
-# def fib(n):
-#     a = 0
-#     b = 1
-#
-#     if n == 1:
-#         print(a)
-#     else:
-#         print(a)
-#         print(b)
-#
-#         for i in range(2, n):
-#             c = a + b
-#             a = b
-#             b = c
-#             print(c)
-#
-# # Call the function with the desired number of terms
-# fib(10)  # You can replace 5 with any positive integer
+def fib(n):
+    a = 0
+    b = 1
 
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+            print(c)
+# Call the function 
+fib(10)  # You can replace 10 with any positive integer ypu want 
+
+#*******-*-*-*-*-*-***-Same Code Using User Input*-*-*-*-*-*-*-*-***----*****---***--***---****---****---***--
 # using user input
-# a = 0
-# b = 1
-# num = int(input("enter the number to obtain Fibonacci series: "))
-# if num == 1:
-#     print(a)
-# else:
-#     print(a)
-#     print(b)
-#     for i in range(2, num):
-#         c = a+b
-#         a=b
-#         b=c
-#         print(c)
+a = 0
+b = 1
+num = int(input("enter the number to obtain Fibonacci series: "))
+if num == 1:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2, num):
+        c = a+b
+        a=b
+        b=c
+        print(c)
