0891385f9f6a0448242ee17239446bc2c1f0ed66
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-02-10 01:21:40 +0530

    Basic_List_concepts.py

--- Basic_List_concepts.py
+++ Basic_List_concepts.py
@@ -2,23 +2,26 @@
 # A list in Python is a collection of items that are ordered, changeable (mutable), and allow duplicate values.
 # Lists are one of the most commonly used data types in Python because they are versatile and easy to use.
 #The first item has an index of 0, the second has an index of 1, and so on.
-
-numbers = [10,0,-1,7,10,85]
-#names=["nasir","Python","Java","Django"]
+#********----------***********----------------**********--------************---------********----*********-*-*-*-*-
+numbers = [10,0,-1,7,10,85] # list name if numbers here you can take any name as your choice 
 #mix_list=["school",1,"college",2,"Graduate",8.1,"complete"] # So List can contain multiple data type in it
-#print(len(numbers))  # len is use to calculate tha length of data present in it
+#print(len(numbers))   # len is use to calculate tha length of data present in it
 #print(max(numbers))   # Give the largest number present in numbers list
 #print(min(numbers))   # Give tha Smallest number
 #print(numbers.sort()) # It will give You None so don't code in this type
 #numbers.sort()        # Code like this to sort numbers
 #print(numbers)
-#numbers.reverse()      # Code for reverse list you also use -1 either to reverse
+#numbers.reverse()     # Code for reverse list you also use -1 either to reverse
 #print(numbers)
-#numbers.append(50)      # this function will add 50 at end in numbers
+#numbers.append(50)    # this function will add 50 at end in numbers only 1 number we can add using append
 #print(numbers)
-# if you want to add numbers at specific index then mention index number
+# if you want to add numbers at specific index then mention index number****----****-----**--
 #numbers.insert(1,30) # Insert functions is used to add numbers at any index append only add data at end
 #print(numbers)
+# If You want to add more than 1 numbers use extend keyword method*****-----****---
+numbers = [10,0,-1,7,10,85]  
+numbers.extend([22,23,100]) # Here We Added more  numbers in the list 
+print(numbers)
 #print(numbers[0:5])   # Print from index 0 till 5  Output : 10,0,-1,7,10
 #print(numbers[1:5])   # print from index 1 till 5  Output : 0,-1,7,10
 #print(numbers[0:5:2]) # it will jump 2 step Output : 10, -1, 10
