a99e223099e5223b4ce698e7197c7bc672357819
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-02-10 19:44:09 +0530

    Math.Module

---
+++ Math_function.py
@@ -0,0 +1,27 @@
+#Python has a built-in module that you can use for mathematical tasks.
+#The math module has a set of methods and constants.
+import math
+x=math.sqrt(25)  # math.square root
+print(x)
+
+y=math.ceil(4.2)  # math.ceil
+print(y)
+
+f=math.floor(5.7)   # math.floor
+print(f)
+
+p=math.pow(2,2)  # Power Function
+print(p)
+
+print(math.pi)    # pie
+
+fact=math.factorial(6)  # Factorial of number
+print(fact)
+
+#******------********-------*****---****Same Codes****---****---***---****----****----****----****----****----****----****
+# import math as m    # Importing math as m we use M instead of Math word
+# print(m.sqrt(25))     # So Here We are Writing only M instead of writing full math.sqrt etc .
+# print(m.pow(2,2))  # Power
+# print(m.ceil(2.2))   # Ceil use floor value
+# print(m.floor(2.6))  # Floor
+# print(m.pi)          # Pie
