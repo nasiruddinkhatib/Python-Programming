5b7cc29bba1781a14ea0c8b343d717d54935b293
Nasir <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-03-20 08:38:16 +0530

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,10 +1,10 @@
-#*********---------************----- Factorial Program********------*********-------*********----**
+#***********---------************----- Factorial Program**********------**********-------************
 # In short, a factorial is a function that multiplies a number by every number below it till 1.
 # For example, the factorial of 3 represents the multiplication of numbers 3, 2, 1, i.e. 3! = 3 × 2 × 1 and is equal to 6.
 def fac(n):
     if n==0:  # Factorial of 0 is 1
         return 1
-    return n*fac(n-1)
+    return n*fac(n-1)     
 result=fac(3)
 print(result)
 
