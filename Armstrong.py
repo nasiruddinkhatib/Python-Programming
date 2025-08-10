2c3da68b9bff697d27283bdd6e133d1fb28be807
Nasir <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-08-10 21:21:00 +0530

    Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -17,3 +17,29 @@ if sum == num:
     print("It is an Armstrong number")  # Output if the number is an Armstrong number
 else:
     print("Not an Armstrong number")    # Output if the number is not an Armstrong number
+
+##################################################################################################
+# Program to print a Armstrong number with 4 number input 
+num = int(input("Enter the number here: "))      # Prompt the user to enter a number
+order =len(str(num))                             
+
+# Initialize variables
+sum = 0        # To store the sum of the cubes of the digits
+temp = num     # Temporary variable to process the number without altering the original
+
+# Loop through each digit of the number
+while temp > 0:
+    digit = temp % 10         # Extract the last digit of the number
+    cube = digit ** order         # Calculate the cube of the digit
+    sum = sum + cube          # Add the cube to the sum
+    temp //= 10               # Remove the last digit from the number
+
+# Check if the sum of the cubes is equal to the original number
+if sum == num:
+    print("It is an Armstrong number")  # Output if the number is an Armstrong number
+else:
+    print("Not an Armstrong number")    # Output if the number is not an Armstrong number
+
+# Output:
+# Enter the number here: 1634
+# It is an Armstrong number
